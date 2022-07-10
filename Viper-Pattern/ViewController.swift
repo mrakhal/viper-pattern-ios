6f8c3db2fc62c3bc25d586f289bd2e6806885102
Mochamad Rakha Luthfi F <mochamadrakha@gmail.com>
2022-07-10 19:05:37 +0700

    Initial Commit

---
+++ Viper-Pattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Viper-Pattern
+//
+//  Created by Mochamad Rakha Luthfi Fahsya on 10/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
